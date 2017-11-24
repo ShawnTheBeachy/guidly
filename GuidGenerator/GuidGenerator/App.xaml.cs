2555d9c384111ab5fd3d74c33246966c8d7b416d
Tastes Like Turkey <esbe118@live.com>
2017-11-24 16:42:05 -0500

    Initial code commit.

---
+++ GuidGenerator/GuidGenerator/App.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.VoiceCommands;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace GuidGenerator
+{
+    sealed partial class App : Application
+    {
+        public App() =>
+            InitializeComponent();
+        
+        protected async override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            ColorTitleBar();
+            var rootFrame = Window.Current.Content as Frame;
+            
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+
+                Window.Current.Activate();
+            }
+
+            await InstallVCDFileAsync();
+        }
+
+        private async Task InstallVCDFileAsync()
+        {
+            if (VoiceCommandDefinitionManager.InstalledCommandDefinitions?.Count > 0)
+                return;
+
+            var vcdFile = await Package.Current.InstalledLocation.GetFileAsync("CortanaCommands.xml");
+            await VoiceCommandDefinitionManager.InstallCommandDefinitionsFromStorageFileAsync(vcdFile);
+        }
+
+        private void ColorTitleBar()
+        {
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            var themeColor = (Color)Resources["SystemAccentColor"];
+            titleBar.BackgroundColor = themeColor;
+            titleBar.ButtonBackgroundColor = themeColor;
+            titleBar.ButtonHoverBackgroundColor = Color.FromArgb(255, 152, 142, 133);
+            titleBar.ButtonPressedBackgroundColor = Color.FromArgb(255, 113, 106, 100);
+            titleBar.ForegroundColor = Colors.White;
+            titleBar.ButtonForegroundColor = Colors.White;
+        }
+    }
+}
---
+++ GuidGenerator/GuidGenerator/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace GuidGenerator
+{
+    public sealed partial class MainPage : Page
+    {
+        #region CurrentGuid
+
+        public Guid CurrentGuid
+        {
+            get => (Guid)GetValue(CurrentGuidProperty);
+            set => SetValue(CurrentGuidProperty, value);
+        }
+        
+        public static readonly DependencyProperty CurrentGuidProperty =
+            DependencyProperty.Register("CurrentGuid", typeof(Guid), typeof(MainPage), new PropertyMetadata(Guid.NewGuid()));
+
+        #endregion CurrentGuid
+
+        private bool _isCompactView = false;
+
+        public MainPage() => 
+            InitializeComponent();
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e) =>
+            CopyGuid(sender);
+
+        private void CopyGuid(object sender)
+        {
+            var dataPackage = new DataPackage
+            {
+                RequestedOperation = DataPackageOperation.Copy
+            };
+            dataPackage.SetText(CurrentGuid.ToString());
+            Clipboard.SetContent(dataPackage);
+            CopiedFlyout.ShowAt(sender as FrameworkElement);
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentGuid = Guid.NewGuid();
+            CopyGuid(sender);
+        }
+
+        private async void MainPanel_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(_isCompactView ? ApplicationViewMode.Default : ApplicationViewMode.CompactOverlay);
+            _isCompactView = !_isCompactView;
+        }
+    }
+}
